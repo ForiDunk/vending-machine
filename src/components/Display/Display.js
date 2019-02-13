51928b7dbad2ba45c7017b695c542604ac4b8129
bela-dev <fori-prg@pathwwway.com>
2019-02-13 16:10:20 +0200

    css tests

---
+++ src/components/Display/Display.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as styles from './Display.module.css';
+
+const display = () => (
+  <div className={styles.display}>
+    <p className={styles.display__text}>Cocktail</p>
+  </div>
+);
+
+export default display;
--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import styles from './Board.module.css';
+import Display from '../../components/Display/Display';
 
 class Board extends Component {
   render() {
     return (
       <div className={styles.board}>
-        <div>Display</div>
+        <Display />
         <div>Keypad</div>
         <div>AddMoney</div>
       </div>
