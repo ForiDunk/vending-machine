a17122f84d209c42184bf34bd7fa6b38058c861e
ForiDunk <Szabolcs.forreiter@gmail.com>
2019-02-16 15:58:36 +0200

    small fix

--- src/components/Product/svgs/index.js
+++ src/components/Product/svgs/index.js
@@ -8,19 +8,7 @@ import Juice from './juice.svg';
 import LemonTea from './lemon-tea.svg';
 import Margarita from './margarita.svg';
 import MilkBottle from './milk-bottle.svg';
-import Milk from './milk.svg';
-import MineralWater from './mineral-water.svg';
-import Mojito from './mojito.svg';
-import RootBeer from './root-beer.svg';
-import Smoothie from './smoothie.svg';
-import SoftDrink from './soft-drink.svg';
-import SoyMilk from './soy-milk.svg';
-import Soya from './soya.svg';
-import Tea from './tea.svg';
-import Vodka from './vodka.svg';
-import Wine from './wine.svg';
 
 export default [
   Beer, Cocktail, CoffeeCup, EnergyDrink, GreenTea, IceTea, Juice, LemonTea, Margarita, MilkBottle,
-  Milk, MineralWater, Mojito, RootBeer, Smoothie, SoftDrink, SoyMilk, Soya, Tea, Vodka, Wine
 ];
